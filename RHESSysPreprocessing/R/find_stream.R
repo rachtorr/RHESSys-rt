52957283d44ec41a2c958de22859400f4b491c9f
Rachel Torres <r_torres@ucsb.edu>
2019-04-17 13:42:24 -0700

    updated find stream

--- RHESSysPreprocessing/R/find_stream.R
+++ RHESSysPreprocessing/R/find_stream.R
@@ -25,7 +25,6 @@ find_stream<-function(flw,road_width){
           desc<-seq.int(1,len_flw)[clist_bool]
         }
 
-
       if(desc[1] > 0){
         for (j in desc) {
           check_lu = c(flw[[j]]$Landtype, check_lu)
@@ -34,22 +33,23 @@ find_stream<-function(flw,road_width){
             strm_chld<-c(strm_chld,j)
             # find 2d distance between two nodes
             strm_dist<-c(strm_dist,sqrt((flw[[i]]$Centroidx-flw[[j]]$Centroidx)^2+(flw[[i]]$Centroidy-flw[[j]]$Centroidy)^2))
-              }
             }
           }
         }
-
         if(is.null(strm_chld)){
           strm_chld<-bottoms
           # find 2d distance between two nodes
           strm_dist<-c(strm_dist,sqrt((flw[[i]]$Centroidx-flw[[bottoms]]$Centroidx)^2+(flw[[i]]$Centroidy-flw[[bottoms]]$Centroidy)^2))
         }
-      strm_chld<-strm_chld[order(strm_dist)] # order list by 2d distance
-      flw[[i]]$Roadtype<-strm_chld[[1]]  #pick closest one
+        strm_chld<-strm_chld[order(strm_dist)] # order list by 2d distance
+        flw[[i]]$Roadtype<-strm_chld[[1]]  #pick closest one
+        }
+
+
 
   }
   for (i in 1:len_flw){
-    if (flw[[i]]$Roadtype!=0){
+    if (flw[[i]]$Roadtype > 0){
       strm<-flw[[i]]$Roadtype
       flw[[i]]$Landtype<-2
       flw[[i]]$Roadtype<-c(flw[[strm]]$PatchID,flw[[strm]]$ZoneID,flw[[strm]]$HillID,road_width)
