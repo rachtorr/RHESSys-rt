7b288a1b4a1e323f6a997cedac15bf58b763687c
Rachel Torres <r_torres@ucsb.edu>
2019-04-17 10:13:09 -0700

    removed redundant loop

--- RHESSysPreprocessing/R/find_stream.R
+++ RHESSysPreprocessing/R/find_stream.R
@@ -5,9 +5,13 @@ find_stream<-function(flw,road_width){
   len_flw<-length(flw)
   bottoms = c()
   for (i in 1:len_flw){
-    # find basin outlet (which could also be a stream) and assign it to var bottoms
+    # find basin outlet, which should be a stream) and assign it to var bottoms
     if(flw[[i]]$Landtype==1){bottoms = c(bottoms,i)}
   }
+
+  strm_chld<-c()  #list of descendants with a stream or bottom
+  strm_dist<-c()  # 2d distance to each stream
+  check_lu <- c() # list of
 
   for (i in 1:len_flw){
     if ((flw[[i]]$Roadtype==1)&(sum(flw[[i]]$Gamma_i)!=0)){
@@ -21,34 +25,25 @@ find_stream<-function(flw,road_width){
           desc<-seq.int(1,len_flw)[clist_bool]
         }
 
-      strm_chld<-c()  #list of descendants with a stream or bottom
-      strm_dist<-c()  # 2d distance to each stream
-      check_lu <- c()
 
-      if(desc > 0){
+      if(desc[1] > 0){
         for (j in desc) {
           check_lu = c(flw[[j]]$Landtype, check_lu)
-          if ((flw[[j]]$Landtype==1)| (sum(flw[[j]]$Gamma_i)==0)) { #has a stream or is bottom
+          if ((flw[[j]]$Landtype==1) | flw[[j]]$Landtype==2 | (sum(flw[[j]]$Gamma_i)==0)) {
+            #has a stream, or there's a downstream road, or is bottom
             strm_chld<-c(strm_chld,j)
             # find 2d distance between two nodes
             strm_dist<-c(strm_dist,sqrt((flw[[i]]$Centroidx-flw[[j]]$Centroidx)^2+(flw[[i]]$Centroidy-flw[[j]]$Centroidy)^2))
-          }
-          if(sum(check_lu)==0){
-            if((flw[[j]]$Landtype==2)){
-              strm_chld<-c(strm_chld,j)
-              # find 2d distance between two nodes
-              strm_dist<-c(strm_dist,sqrt((flw[[i]]$Centroidx-flw[[j]]$Centroidx)^2+(flw[[i]]$Centroidy-flw[[j]]$Centroidy)^2))
-            }
-          if(is.null(strm_chld)){
-            strm_chld<-bottoms
-            # find 2d distance between two nodes
-            strm_dist<-c(strm_dist,sqrt((flw[[i]]$Centroidx-flw[[bottoms]]$Centroidx)^2+(flw[[i]]$Centroidy-flw[[bottoms]]$Centroidy)^2))
-          }
-
+              }
             }
           }
         }
-    }
+
+        if(is.null(strm_chld)){
+          strm_chld<-bottoms
+          # find 2d distance between two nodes
+          strm_dist<-c(strm_dist,sqrt((flw[[i]]$Centroidx-flw[[bottoms]]$Centroidx)^2+(flw[[i]]$Centroidy-flw[[bottoms]]$Centroidy)^2))
+        }
       strm_chld<-strm_chld[order(strm_dist)] # order list by 2d distance
       flw[[i]]$Roadtype<-strm_chld[[1]]  #pick closest one
 
